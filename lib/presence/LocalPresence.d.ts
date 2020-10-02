8fd3f062a631e7b20c89e9aed217e39a5f236b78
rahmatn <rahmat.n@shopee.com>
2020-10-02 17:10:01 +0700

    :fire:

---
+++ lib/presence/LocalPresence.d.ts
@@ -0,0 +1,36 @@
+/// <reference types="node" />
+import { Presence } from './Presence';
+import { EventEmitter } from 'events';
+export declare class LocalPresence implements Presence {
+    channels: EventEmitter;
+    private listenersByTopic;
+    data: {
+        [roomName: string]: string[];
+    };
+    hash: {
+        [roomName: string]: {
+            [key: string]: string;
+        };
+    };
+    keys: {
+        [name: string]: string | number;
+    };
+    private timeouts;
+    subscribe(topic: string, callback: (...args: any[]) => void): this;
+    unsubscribe(topic: string): this;
+    publish(topic: string, data: any): this;
+    exists(roomId: string): Promise<boolean>;
+    setex(key: string, value: string, seconds: number): void;
+    get(key: string): string | number;
+    del(key: string): void;
+    sadd(key: string, value: any): void;
+    smembers(key: string): Promise<string[]>;
+    srem(key: string, value: any): void;
+    scard(key: string): number;
+    hset(roomId: string, key: string, value: string): void;
+    hget(roomId: string, key: string): Promise<string>;
+    hdel(roomId: string, key: any): void;
+    hlen(roomId: string): Promise<number>;
+    incr(key: string): Promise<string | number>;
+    decr(key: string): Promise<string | number>;
+}
